a69902cec437b45d46fb91e7bf9d57a10bf96462
Dan Mahara <intern.rewasoft@gmail.com>
2025-04-05 12:17:32 +0545

    Removed commented code

--- app/src/main/java/com/example/practicemain/RecyclerViewActivity.java
+++ app/src/main/java/com/example/practicemain/RecyclerViewActivity.java
@@ -18,14 +18,7 @@ public class RecyclerViewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_recycler_view);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
-
         recyclerView = findViewById(R.id.recycler_view);
         // we set layout managers
 
