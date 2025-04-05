730edf2afce2065437eb33de7cf560e401962365
Dan Mahara <intern.rewasoft@gmail.com>
2025-04-05 12:15:28 +0545

    Cleanup RecyclerViewActivity and update UI

--- app/src/main/java/com/example/practicemain/RecyclerViewActivity.java
+++ app/src/main/java/com/example/practicemain/RecyclerViewActivity.java
@@ -1,16 +1,14 @@
 package com.example.practicemain;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RecyclerViewActivity extends AppCompatActivity {
@@ -20,13 +18,13 @@ public class RecyclerViewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_recycler_view);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
 
         recyclerView = findViewById(R.id.recycler_view);
         // we set layout managers
@@ -41,23 +39,17 @@ public class RecyclerViewActivity extends AppCompatActivity {
 //        recyclerView.setLayoutManager(linearLayoutManager);
 
         recyclerView.setLayoutManager(gridLayoutManager);
-//        userDataList = new ArrayList<>();
-//        for (int i = 1; i <=10; i++) {
-//            userDataList.add(new UserData("name " + i, "email " + i, i, i));
-//        }
-
-        DBConnect dbConnect = new DBConnect(this);
+        userDataList = new ArrayList<>();
         for (int i = 1; i <= 5; i++) {
-            dbConnect.insertUser(new UserData("name " + i, "email " + i, i, i));
+            userDataList.add(new UserData("name " + i, "email " + i, i, i));
         }
-
-        dbConnect.deleteUser(5);
-        dbConnect.updateUser(4, new UserData("Dan Mahara", "dan@gmail.com", 5, 5));
-
-//        CustomRecyclerViewAdapter adapter = new CustomRecyclerViewAdapter(userDataList);
-        CustomRecyclerViewAdapter adapter = new CustomRecyclerViewAdapter(dbConnect.retriveUsers());
+        CustomRecyclerViewAdapter adapter = new CustomRecyclerViewAdapter(userDataList, new UserDataCallback() {
+            @Override
+            public void onItemClicked(UserData userData) {
+                Toast.makeText(RecyclerViewActivity.this, userData.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
         recyclerView.setAdapter(adapter);
-
 
 
     }
